7b9b01d0e12900b8ee0ef0c2a09eecfc9f00158e
sxfang <sxfang@cam2cloud.com>
2018-06-28 14:49:26 +0800

    feat(arguments): add arguments support

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-export const run=(exec)=>{
-    var file=new File(['this.addEventListener("message",(event)=>{try{var result=eval("("+event.data+")()");this.postMessage(result)}catch(err){this.postMessage(err)}})'],`${Date.now()+parseInt(Math.random()*10000)}.js`,{
+export const run=(exec,...args)=>{
+    var file=new File([`try{var result=eval("(${exec.toString()})(${args})");this.postMessage(result)}catch(err){this.postMessage(err)}`],`${Date.now()+parseInt(Math.random()*10000)}.js`,{
       type: "application/javascript",
     })
     var url=URL.createObjectURL(file)
     var worker=new Worker(url)
     return new Promise((resolve,reject)=>{
-        worker.postMessage(exec.toString())
         worker.addEventListener('message',(event)=>{
              worker.terminate()
              delete worker
